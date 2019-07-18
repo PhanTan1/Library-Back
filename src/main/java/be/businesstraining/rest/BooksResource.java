a1661688e620039f1be1a042b193a2851b2140de
Tan Phan <p.tanvu@gmail.com>
2019-07-18 17:10:09 +0200

    update

---
+++ src/main/java/be/businesstraining/rest/BooksResource.java
@@ -0,0 +1,9 @@
+package be.businesstraining.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trips")
+public class BooksRessource {
+}

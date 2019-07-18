e406b7131de90c0503781aa2d78bd891da8295e3
Tan <p.tanvu@gmail.com>
2019-07-18 11:41:56 +0200

    Commit initial du back

---
+++ src/main/java/be/businesstraining/LibrarytanApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarytanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarytanApplication.class, args);
+    }
+
+}
